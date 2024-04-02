69eaa21635a3d6bbb91c59ca0efadf521a212c70
Vinicius Matheus Sary de Lima <vinicius.sary@pucpr.edu.br>
2024-04-02 18:38:23 -0300

    pet done

---
+++ Pet.java
@@ -0,0 +1,20 @@
+public class Pet {
+    private String nome;
+    private String tipo;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String novoNome){
+        this.nome = novoNome;
+    }
+
+    public String getTipo(){
+        return this.tipo;
+    }
+
+    public void setTipo(String novoTipo){
+        this.nome = novoTipo;
+    }
+}
