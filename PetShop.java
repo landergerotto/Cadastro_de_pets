942adb57ec8dcf8929e9e2ba59fa2595b3a6ad85
Vinicius Matheus Sary de Lima <vinicius.sary@pucpr.edu.br>
2024-04-02 19:41:08 -0300

    scanner error

--- PetShop.java
+++ PetShop.java
@@ -6,7 +6,8 @@ import java.time.Period;
 
 public class PetShop {
 
-    public ArrayList<Tutor> tutores = new ArrayList<Tutor>();
+    public static ArrayList<Tutor> tutores = new ArrayList<Tutor>();
+    public static int lastid = 0;
 
     public static void main(String[] args) {
         while(true) {
@@ -25,8 +26,50 @@ public class PetShop {
         );
     } 
 
-    public static void CadastrarTutor() {
-        
+    public static void Cadastrar() {
+        System.out.println("CHAMOI");
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.println("Digite nome do tutor (vazio encerra cadastro tutor):");
+        System.out.print("> ");
+        String nome = scanner.nextLine();
+    
+        // System.out.println("Digite dia (dd), mês (mm) e ano (aaaa) de nascimento do tutor: \n" +
+        //         "(separados por espaços)");
+        // System.out.print("> ");
+        // String data = scanner.nextLine();
+        // if (data.isEmpty()) {
+        //     scanner.close();
+        //     return;
+        // }
+    
+        // String[] dateParts = data.split(" ");
+        // int ano, mes, dia;
+        // try {
+        //     ano = Integer.parseInt(dateParts[0]);
+        //     mes = Integer.parseInt(dateParts[1]);
+        //     dia = Integer.parseInt(dateParts[2]);
+        // } catch (NumberFormatException e) {
+        //     System.out.println("Invalid date format.");
+        //     scanner.close();
+        //     return;
+        // }
+    
+        // System.out.println("Digite endereço do tutor/pet: ");
+        // System.out.print("> ");
+        // String endereco = scanner.nextLine();
+        // if (endereco.isEmpty()) {
+        //     scanner.close();
+        //     return;
+        // }
+    
+        // scanner.close();
+    
+        // int codTutor = lastid;
+        // lastid++;
+    
+        // Tutor tutor = new Tutor(codTutor, nome, ano, mes, dia, endereco);
+        // tutores.add(tutor);
     }
 
     public static void ImprimirCadastro() {
@@ -49,7 +92,7 @@ public class PetShop {
 
         switch(opcao) {
             case "c":
-                CadastrarTutor();
+                Cadastrar();
                 break;
 
             case "i":
